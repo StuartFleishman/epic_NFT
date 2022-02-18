8093dccf23d8573a3bfda4a2c552988f35e758ac
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-02-18 15:03:44 -0500

    importing ERC721 standard from openZepplin

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,11 +1,33 @@
-// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
-pragma solidity ^0.8.1;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
-        console.log("This is my NFT contract. Whoa!");
-    }
+
+contract MyEpicNFT is ERC721URIStorage {
+  
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+
+  constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("This is my NFT contract. Woah!");
+  }
+
+  
+  function makeAnEpicNFT() public {
+     
+    uint256 newItemId = _tokenIds.current();
+
+     
+    _safeMint(msg.sender, newItemId);
+
+  
+    _setTokenURI(newItemId, "blah");
+
+   
+    _tokenIds.increment();
+  }
 }
